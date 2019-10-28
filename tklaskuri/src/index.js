497b96a5f2365f64e1aeb9a40f60c6adaac430ea
Aleksi Tarakkamäki <aleksi.tarakkamaki@gmail.com>
2019-10-28 15:32:11 +0200

    Toimiva valintalista

--- tklaskuri/src/index.js
+++ tklaskuri/src/index.js
@@ -8,28 +8,35 @@ const App = () => {
     
     
     const valuet = data.dataset.dimension["Alue 2019"].category.label
+    const asukasmaara = data.dataset.value
+
 
     var taulukko = [];
     var x;
-
+    var listaIndex = 0;
     
 
     for (x in valuet) {
         taulukko.push(valuet[x]);
       }
 
+    var indexLista = 0
+    const tulosta = (listaValittu) => {
+    	console.log(listaIndex)
+    	listaIndex = listaValittu.target.value
+
+    }
     
-    
-    
-  
+    console.log(listaIndex)
     return (
       <div>
         
         
-          <ul>
-          {taulukko.map(s => (<li>{s}</li>))}
+          <select id="listaKunnista" size="25" onChange={tulosta}>
+          {taulukko.map(s => (<option value={indexLista++}>{s}</option>))}
+
                 
-            </ul>
+            </select>
         
         <p>
         
@@ -40,6 +47,8 @@ const App = () => {
         
       </div>
     )
+
+    
   }
 
 ReactDOM.render(

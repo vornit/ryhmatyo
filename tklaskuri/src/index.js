d9c1659e5b969bde2f0c1d0bbb8ac07157029e3c
Tuomo <tuomo.vitikainen@gmail.com>
2019-10-31 12:57:46 +0200

    vaakunahomma

--- tklaskuri/src/index.js
+++ tklaskuri/src/index.js
@@ -9,12 +9,10 @@ import datavaakunat from "./vaakunaKuvat"
 const App = () => {
     
     
-  const [ asindeksi, setCounter ] = useState(0)
+  const [ counter, setCounter ] = useState(0)
 
-
-  const setToValue = (value) =>
-   { setCounter(value) }
-
+  const setToValue = (value) => setCounter(value)
+  console.log(counter)
 
   	//objektilista kuntien nimistä
     const kuntienNimet = data.dataset.dimension["Alue 2019"].category.label
@@ -23,14 +21,23 @@ const App = () => {
     //objektilista kuntien indekseistä
     const kuntienIndeksit = data.dataset.dimension["Alue 2019"].category.index
 
+    const vaakunat = datavaakunat.selection1
+
+    console.log(vaakunat[5].image)
+
     var nimiTaulukko = [];
     var kuntienIit = [];
+    var vaakunaTaulukko = [];
+
+
     
     
     // kuntien nimet taulukkoon
     for (var x in kuntienNimet) {
         nimiTaulukko.push(kuntienNimet[x]);
     }
+
+ 
 
     // kuntien indeksit taulukkoon
     for (var x in kuntienIndeksit) {
@@ -68,7 +75,7 @@ const App = () => {
       listaIndex = listaValittu.target.value
       console.log(listaIndex)
       setToValue(listaIndex)
-      
+      console.log(counter)
     }
     
     // asukasluvut löytyvät taulukosta neljän indeksin välein ([0,4,8,...])
@@ -76,16 +83,14 @@ const App = () => {
     // väkiluvunmuutos löytyvät taulukosta kolmen indeksin välein
     var vakiluvunMuutos = -3;
     // valintalista kunnista, indeksöi samalla 0->n
-
     return (
-
-<div className="container">	
+      <div className="container">	
 
 
 <div class="row justify-content-md-center">
     
 
-  <div class="btn-group btn-group-lg">
+<div class="btn-group btn-group-lg">
     <button type="button" class="btn btn-primary" aria-pressed="true">Toimialat</button>
     <button type="button" class="btn btn-primary" aria-pressed="true">Paikkakunnat</button>
     
@@ -94,60 +99,29 @@ const App = () => {
   </div>
 
         <div className="row">
-          <div className="col-sm">
-            
-            <select id="listaKunnista" className="form-control" size="24" onChange={tulosta}>
-            {nimetJarjestyksessa.map(s => (<option value={indexLista++}>{s}</option>))} 
-            </select>
-            </div>
-
-            <div className="col-sm">
-
-            <div class="col-sm">
-
-            <h3>{jarjestetty[asindeksi]}</h3>
-
-            Kunnan asukasluku: <small class="text-muted"> {asukasmaarat[asindeksi]} </small>
-
-            </div>
-
-
+        <div className="col-sm">
           
-      
-          
-
-          <div class="col-sm">
-
-          <h3>Vertailu</h3>
-          </div>
-
-
           <select id="listaKunnista" className="form-control" size="25" onChange={tulosta}>
           {nimetJarjestyksessa.map(s => (<option value={asukaslukuInd+=4}>{s}</option>))} 
           </select>
           </div>
 
           <div className="col-sm">
+            
          Kunnan asukasluku: {pktiedot[counter]}
+
+         <img 
+      src={vaakunat[counter].image}
+      alt="new"
+      />
+
         </div>
         <div className="col-sm">
          Väkiluvun muutos edellisestä vuodesta prosentteina: {pktiedot[counter]}
         </div>
 
-
-
-          </div>
-
-  </div>	
-
-  <div class="footer">
-  <p>TIEA207 kurssin projektityö</p>
-</div>
-
-</div>
-
-
-
+        </div>		
+      </div>
     )
 
     

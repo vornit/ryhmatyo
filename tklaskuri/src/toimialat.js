8ca18f0d065384d8ba4e06755591ba79da0ec6e7
Aleksi Tarakkamäki <aleksi.tarakkamaki@gmail.com>
2019-11-19 15:33:14 +0200

    perkele

--- tklaskuri/src/toimialat.js
+++ tklaskuri/src/toimialat.js
@@ -16,7 +16,7 @@ const toimialojenPaastot = dataPaastot.dataset.value
 const toimialojenPaastotIndeksit = dataPaastot.dataset.dimension["Toimialat (TOL2008) ja kotitaloudet"].category.index;
 
 
-
+console.log(datatoimialatKunnittain)
 const Toimialat = () => {
 
 //console.log(verotaulukko)
@@ -28,21 +28,76 @@ const Toimialat = () => {
   var verotaulukko = [];
   var alataulukko = [];
   var maarataulukko = [];
+  var value = [];
   var paastotaulukko = [];
+  var toimialojenAvaimet = [];
+
+  function toimialanPaikkakunnat(toimiala) {
+
+    var toimialaInt = parseInt(toimiala)
+    var toimialojenLkm = Object.keys(toimialalista).length
+    console.log(toimialojenLkm)
+    for (let i = toimialaInt; i < toimialojenMaarat.length; i = (i+toimialojenLkm)){
+      value.push(toimialojenMaarat[i]);
+    }
+    
+  }
+
+  function etsiIsoin(){
+    var enitenKunnassa = [];
+    var kunnanAvain = [];
+    var suurin = 0;
+    var maxIndex = 0;
+
+    for(let i = 1; i < value.length; i++){
+      if(value[i] > suurin){
+        maxIndex = i;
+        suurin = value[i];
+        if(enitenKunnassa.length < 5){
+          enitenKunnassa.push(suurin)
+          kunnanAvain.push(maxIndex)
+        }
+        if(enitenKunnassa.length == 5){
+          enitenKunnassa[4] = enitenKunnassa[3]
+          enitenKunnassa[3] = enitenKunnassa[2]
+          enitenKunnassa[2] = enitenKunnassa[1]
+          enitenKunnassa[1] = enitenKunnassa[0]
+          enitenKunnassa[0] = suurin
+
+          kunnanAvain[4] = kunnanAvain[3]
+          kunnanAvain[3] = kunnanAvain[2]
+          kunnanAvain[2] = kunnanAvain[1]
+          kunnanAvain[1] = kunnanAvain[0]
+          kunnanAvain[0] = maxIndex
+
+        }
+        
+      }
+      
+      console.log("eniten kunnassa " + enitenKunnassa)
+      console.log("kunnan avain " + kunnanAvain)
+    }
+
+  }
 
   function luoTaulukot() {
-    
+   // var toimialojenLkm = Object.keys(toimialat).length
     for (let key in toimialalista){
       if(key.length == 2){
+
+     //   for (let i = key; i < toimialojenLkm; ()) {
+      //    value.push(toimialojenMaarat[i]);
+       // }
+        toimialojenAvaimet.push(key)
         alataulukko.push(toimialalista[key])
         maarataulukko.push(toimialojenMaarat[toimialaIndeksit[key]])
         paastotaulukko.push(toimialojenPaastot[toimialojenPaastotIndeksit[key]])
         if (toimialojenVerot[nimiJaIndeksi[key]] == null){
-
+          
           verotaulukko.push("Ei tiedossa")
         } 
         else {
-        
+          
          verotaulukko.push(toimialojenVerot[nimiJaIndeksi[key]])
         }
       } 
@@ -83,6 +138,8 @@ const Toimialat = () => {
    const tulostaToimiala = (listaValittu) => {
    
    setToValue(listaValittu.target.value)
+   toimialanPaikkakunnat(listaValittu.target.value)
+   etsiIsoin(listaValittu.target.value)
    
    
  }
@@ -92,6 +149,7 @@ const Toimialat = () => {
   if (x == undefined) return "Ei tiedossa";
   else return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
+  console.log("maarataulukko 1 " + maarataulukko)
 
  return (
   // Bootstrapin pääcontainer
@@ -129,6 +187,7 @@ const Toimialat = () => {
             <p>Toimialan kokonaispäästöt: {lukupilkuilla(paastotaulukko[counter])}</p>
             <p>Toimialojen kokonaislukumäärä: {lukupilkuilla(maarataulukko[counter])}</p>
             <p>Toimialan verot yhteensä: {lukupilkuilla(verotaulukko[counter])} €</p>
+            <p>value[counter]</p>
             
             </div>
             </div>

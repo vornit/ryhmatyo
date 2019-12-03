5d1a984fa2075a53aebbd1706529a50676cfd93b
Tuomo <tuomo.vitikainen@gmail.com>
2019-12-03 14:30:12 +0200

    joo

---
+++ tklaskuri/src/aloitus.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+
+
+
+//Pääkomponentti Aloitukselle
+const Aloitus = () => {
+
+  
+  
+  
+
+ 
+ 
+ return (
+  // Bootstrapin pääcontainer
+  <div className="containeraloitus">   
+  
+      <a1>PÄÄSTÖAPP</a1>
+      <br></br>
+      <a2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</a2>
+
+    </div>
+
+        );
+  }
+
+
+export default Aloitus;

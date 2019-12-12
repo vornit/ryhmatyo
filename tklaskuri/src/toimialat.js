429741129be1c013e4de5af1f8835a5672609dab
Aleksi Tarakkamäki <aleksi.tarakkamaki@gmail.com>
2019-12-12 11:57:57 +0200

    mediaani korjaus

--- tklaskuri/src/toimialat.js
+++ tklaskuri/src/toimialat.js
@@ -221,7 +221,7 @@ const Toimialat = () => {
   if (toimialojenSLtaulukko[counter] !== "Ei tiedossa"){
     for ( let i = 0; i < suhdeluvutJarj.length; i++){
       if (suhdeluvutJarj[i].slIndeksi == counter){
-        sijaTulostus = i + 1;
+        sijaTulostus = i + 1 + "./" + suhdeluvutJarj.length + ". | ";
       }
     }
   }
@@ -229,7 +229,23 @@ const Toimialat = () => {
   if (toimialojenSLtaulukko[counter] !== "Ei tiedossa" && sijaTulostus !== "Ei tiedossa"){
     for (let i = 0; i < suhdeluvutJarj.length; i++){
       if (suhdeluvutJarj[i].slIndeksi == counter){
-        mediaaniTulostus = (suhdeluvutJarj[i].suhde/suhdeluvutJarj[suhdeluvutJarj.length/2].suhde).toFixed(2) + "% mediaanista"
+        let mediaaniArvo = suhdeluvutJarj[mediaaniIndeksi].suhde
+        let verrattavaArvo = suhdeluvutJarj[i].suhde
+        
+      if (verrattavaArvo < mediaaniArvo) {
+        mediaaniTulostus = ((mediaaniArvo - verrattavaArvo) / verrattavaArvo) * 100
+        mediaaniTulostus = mediaaniTulostus * (-1)
+        mediaaniTulostus = lukupilkuilla(mediaaniTulostus.toFixed(2))
+        mediaaniTulostus += "% mediaanista"
+      }
+      else if (verrattavaArvo > mediaaniArvo) {
+        mediaaniTulostus = ((verrattavaArvo - mediaaniArvo) / mediaaniArvo) * 100
+        mediaaniTulostus = lukupilkuilla(mediaaniTulostus.toFixed(2))
+        mediaaniTulostus = "+" + mediaaniTulostus + "% mediaanista"
+      }
+      else {
+        mediaaniTulostus = "Mediaani"
+      }
       }
     }
   }
@@ -360,7 +376,7 @@ const Toimialat = () => {
                   <li class="list-group-item"><small class="text-muted">Toimialojen kokonaislukumäärä: </small> {lukupilkuilla(maarataulukko[counter])} kpl</li>
                   <li class="list-group-item"> <small class="text-muted">Toimialan verot yhteensä: </small> {veroTulostus}</li>
                   <li class="list-group-item"> <small class="text-muted">Toimialaa eniten paikkakunnalla: </small> {kuntienNimet[kunnanNimiAvain]} Lkm: {kuntienToimialaLkm[kuntienIndeksit[kunnanNimiAvain]]} kpl</li>
-                  <li class="list-group-item"> <small class="text-muted">Toimialan sijoitus ekologisuuden mukaan: </small> {sijaTulostus}. / 86. {mediaaniTulostus}</li>
+                  <li class="list-group-item"> <small class="text-muted">Toimialan sijoitus ekologisuuden mukaan: </small> {sijaTulostus}{mediaaniTulostus}</li>
 
                 </ul>
 
